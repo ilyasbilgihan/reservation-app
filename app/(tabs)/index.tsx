f37d011a67cd145ca21c730c24a2c446ba32926b
İlyas Bilgihan <ilyasbilgihan@gmail.com>
2024-09-12 13:21:06 +0300

    add: support for redirecting to /auth when offline

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,31 @@
-import { Stack, Link } from 'expo-router';
-import { StyleSheet } from 'react-native';
+import { Link, router } from 'expo-router';
 
 import * as React from 'react';
 import { Text } from '~/components/ui/text';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { supabase } from '~/utils/supabase';
+
+import { getNetworkStateAsync } from 'expo-network';
+import { useEffect } from 'react';
+
 export default function Home() {
+  useEffect(() => {
+    (async () => {
+      const networkState = await getNetworkStateAsync();
+      if (!networkState.isConnected) {
+        router.replace('/auth');
+      }
+    })();
+  }, []);
+
   return (
     <>
       <Link href="/(other)/test" className="text-red-500">
         asdasd
       </Link>
+      <TouchableOpacity onPress={() => supabase.auth.signOut()}>
+        <Text className="text-red-500">Logout</Text>
+      </TouchableOpacity>
     </>
   );
 }

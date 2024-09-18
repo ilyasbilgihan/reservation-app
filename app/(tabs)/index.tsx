e85abc9f7ea97d69368241849235f9093bd6822f
İlyas Bilgihan <ilyasbilgihan@gmail.com>
2024-09-18 16:25:20 +0300

    add: flash-calendar library

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -10,6 +10,8 @@ import { useGlobalContext } from '~/context/GlobalProvider';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { View } from 'react-native';
 
+import { Calendar, toDateId } from '@marceloterreiro/flash-calendar';
+
 export default function Home() {
   const { session, branch } = useGlobalContext();
 
@@ -31,77 +33,73 @@ export default function Home() {
   );
 
   const [data, setData] = useState<any>(null);
-  const testDB = async () => {
-    /* const { data, error } = await supabase.from('profile').select().eq('id', session?.user.id); */
-    /* const { data, error } = await supabase
-      .from('branch')
-      .upsert({
-        name: 'test',
-        phone: '1234',
-        city: 'istanbul',
-        country: 'turkey',
-        thumbnail: 'test.jpg',
-        reservation_period: '120',
-        owner_id: session?.user.id,
-        sectore: 'Rental',
-      })
-      .select('*'); */
-
-    const { data: branch } = await supabase.from('branch').select('*');
-
-    /* const { data, error } = await supabase
-      .from('service')
-      .upsert({
-        branch_id: branch.id,
-        name: 'insurance',
-        price: '100',
-        time_span: 0,
-      })
-      .select('*'); */
-    /* const { data, error } = await supabase
-      .from('working_hour')
-      .upsert({
-        branch_id: branch.id,
-        day: 'Monday',
-        opening: '10:00',
-        closing: '18:00',
-      })
-      .select('*'); */
-
-    /* const { data, error } = await supabase
-      .from('asset')
-      .upsert({
-        branch_id: branch.id,
-        name: 'Peugeot 408 (34 UA 34)',
-        price: '100000',
-      })
-      .select('*'); */
-
-    /* prevent adding reservations at intersecting times*/
-    const { data, error } = await supabase
-      .from('reservation')
-      .upsert({
-        branch_id: branch![0]?.id,
-        asset_id: 1,
-        customer_id: session?.user.id,
-        start_date: '2024-09-12T03:00:00.000Z',
-        end_date: '2024-09-12T06:00:00.000Z',
-      })
-      .select('*');
-
-    /* const { data, error } = await supabase.from('service').select('*'); */
-    setData(error || data);
-  };
 
   return (
     <SafeAreaView>
       <TouchableOpacity onPress={() => supabase.auth.signOut()}>
         <Text className="text-red-500">Logout</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={testDB}>
-        <Text className="text-4xl text-purple-500">Test DB</Text>
-      </TouchableOpacity>
-      <Text>{JSON.stringify(data, null, 2)}</Text>
+      <View className="p-12">
+        <Calendar
+          theme={{
+            rowMonth: {
+              content: {
+                height: 24,
+                textAlign: 'left',
+                fontSize: 18,
+                fontFamily: 'Quicksand Medium',
+              },
+            },
+            itemDay: {
+              idle: ({ isPressed }) => ({
+                container: {
+                  backgroundColor: 'lightgray',
+                  borderRadius: 8,
+                  aspectRatio: 1,
+                },
+                content: {
+                  color: 'black',
+                  fontSize: 16,
+                },
+              }),
+              today: ({ isPressed }) => ({
+                container: {
+                  backgroundColor: 'gren',
+                  borderRadius: 8,
+                  aspectRatio: 1,
+                },
+                content: {
+                  fontSize: 16,
+                  color: isPressed ? 'white' : 'black',
+                },
+              }),
+              active: ({ isEndOfRange, isStartOfRange }) => ({
+                container: {
+                  backgroundColor: 'yellow',
+                  borderTopLeftRadius: isStartOfRange ? 16 : 0,
+                  borderBottomLeftRadius: isStartOfRange ? 16 : 0,
+                  borderTopRightRadius: isEndOfRange ? 16 : 0,
+                  borderBottomRightRadius: isEndOfRange ? 16 : 0,
+                },
+                content: {
+                  color: 'orange',
+                },
+              }),
+            },
+            itemDayContainer: {
+              activeDayFiller: {
+                backgroundColor: 'blue',
+              },
+            },
+          }}
+          calendarFormatLocale="tr"
+          calendarFirstDayOfWeek="monday"
+          calendarMonthId={toDateId(new Date())}
+          onCalendarDayPress={(dateId) => {
+            console.log(`Clicked on ${dateId}`);
+          }}
+        />
+      </View>
     </SafeAreaView>
   );
 }

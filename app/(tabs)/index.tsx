df4434fab42aa22928c54518d29d6fc421b6bb44
İlyas Bilgihan <ilyasbilgihan@gmail.com>
2024-09-19 17:13:48 +0300

    add: react-native-maps expo-location to select location

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,30 @@
-import { Link, Redirect, router, useFocusEffect } from 'expo-router';
-
-import { Text } from '~/components/ui/text';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-import { supabase } from '~/utils/supabase';
-
-import { getNetworkStateAsync } from 'expo-network';
 import { useCallback, useEffect, useState } from 'react';
-import { useGlobalContext } from '~/context/GlobalProvider';
+import { View, TouchableOpacity, Image, FlatList, StyleSheet, Alert } from 'react-native';
+import { Link, Redirect, router, useFocusEffect } from 'expo-router';
+import { getNetworkStateAsync } from 'expo-network';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
+import { useGlobalContext } from '~/context/GlobalProvider';
+import { supabase } from '~/utils/supabase';
+
+import { Text } from '~/components/ui/text';
+import { Iconify } from '~/lib/icons/Iconify';
+
+const SECTORS = ['Grooming', 'Accommodation', 'Rental', 'Food'];
+import { getSectorItem } from '~/utils/getLabels';
+
+import MapView, { Marker, PROVIDER_GOOGLE, UrlTile } from 'react-native-maps';
+import { Button } from '~/components/ui/button';
+
+import * as Location from 'expo-location';
+import { ScrollView } from 'react-native-gesture-handler';
+
 export default function Home() {
-  const { session, branch } = useGlobalContext();
+  const { session, branch, location, setLocation } = useGlobalContext();
+  const [selectedSector, setSelectedSector] = useState<string>('Grooming');
+  const [centerPosition, setCenterPosition] = useState<any>(null);
+  const [branches, setBranches] = useState<any>([]);
+  const [region, setRegion] = useState<any>();
 
   useEffect(() => {
     (async () => {
@@ -29,13 +43,122 @@ export default function Home() {
     }, [branch])
   );
 
-  const [data, setData] = useState<any>(null);
+  const setCurrentLocation = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      Alert.alert('Permission not granted', 'Please grant permission to access your location');
+      return;
+    }
+
+    let live_location = await Location.getCurrentPositionAsync({});
+    setLocation({
+      ...location,
+      longitude: live_location.coords.longitude,
+      latitude: live_location.coords.latitude,
+    });
+    setRegion({
+      latitude: live_location.coords.latitude,
+      longitude: live_location.coords.longitude,
+      latitudeDelta: 0.001,
+      longitudeDelta: 0.001,
+    });
+  };
+
+  const fetchBranches = async (lat: any, long: any) => {
+    const { data, error } = await supabase.rpc('nearby_branches', {
+      lat,
+      long,
+    });
+
+    if (error) {
+      console.log(error);
+    } else {
+      setBranches(data);
+    }
+  };
 
   return (
     <SafeAreaView>
-      <TouchableOpacity onPress={() => supabase.auth.signOut()}>
-        <Text className="text-red-500">Logout</Text>
-      </TouchableOpacity>
+      <ScrollView>
+        <View className="mx-7 flex-row items-center justify-between pt-7">
+          <TouchableOpacity activeOpacity={0.75} onPress={setCurrentLocation}>
+            <View className="h-12 flex-row items-center gap-2 rounded-xl border border-neutral-100 bg-white px-4">
+              <Iconify icon="solar:map-point-bold-duotone" size={24} className="text-primary" />
+              <Text className="font-qs-semibold text-slate-500">{location.label}</Text>
+            </View>
+          </TouchableOpacity>
+          <View className="overflow-hidden rounded-xl border border-neutral-100 bg-white">
+            <Image source={require('~/assets/no-image.png')} className="h-12 w-12" />
+          </View>
+        </View>
+        <FlatList
+          data={SECTORS}
+          contentContainerStyle={{
+            paddingVertical: 14,
+            paddingHorizontal: 24,
+          }}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              activeOpacity={0.75}
+              onPress={() => setSelectedSector(item)}
+              className={`${selectedSector == item ? 'bg-primary' : 'bg-background'} items-center justify-center rounded-xl border border-neutral-100 px-3.5 py-2.5`}>
+              <View className="flex-row gap-3.5">
+                {getSectorItem(item)?.icon(
+                  selectedSector == item ? 'rgb(237 233 254)' : 'rgb(148 163 184)'
+                )}
+                <Text
+                  className={`${selectedSector == item ? ' text-violet-100' : 'text-slate-400'} font-qs-semibold`}>
+                  {getSectorItem(item)?.value}
+                </Text>
+              </View>
+            </TouchableOpacity>
+          )}
+          ItemSeparatorComponent={() => <View style={{ width: 7 }} />}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          keyExtractor={(item) => 'sector-' + item}
+          /* extraData={selectedId}  // rerender when selectedId changes */
+        />
+        <View className="px-7">
+          <Text>{JSON.stringify(centerPosition, null, 2)}</Text>
+          <View className="aspect-square w-full">
+            {location ? (
+              <MapView
+                showsCompass
+                showsUserLocation
+                initialRegion={{
+                  latitude: location?.latitude,
+                  longitude: location?.longitude,
+                  latitudeDelta: 0.08,
+                  longitudeDelta: 0.08,
+                }}
+                region={region}
+                onRegionChangeComplete={(region) => setCenterPosition(region)}
+                style={styles.map}
+              />
+            ) : null}
+          </View>
+        </View>
+        <View className="px-7">
+          <Button
+            onPress={() => {
+              fetchBranches(centerPosition?.latitude, centerPosition?.longitude);
+            }}>
+            <Text>Get nearby branches</Text>
+          </Button>
+          <Text>{JSON.stringify(branches, null, 2)}</Text>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+});
--- context/GlobalProvider.tsx
+++ context/GlobalProvider.tsx
@@ -4,11 +4,19 @@ import { Session } from '@supabase/supabase-js';
 import { supabase } from '~/utils/supabase';
 import { getItem, setItem } from '~/core/storage';
 
+type Location = {
+  latitude: number;
+  longitude: number;
+  label: string;
+};
+
 export interface GlobalContextValue {
   session: Session | null;
   setSession: React.Dispatch<React.SetStateAction<Session | null>>;
   branch: any;
   setBranch: React.Dispatch<React.SetStateAction<any>>;
+  location: Location;
+  setLocation: React.Dispatch<React.SetStateAction<Location>>;
 }
 
 const GlobalContext = React.createContext<GlobalContextValue>({} as GlobalContextValue);
@@ -16,6 +24,11 @@ const GlobalContext = React.createContext<GlobalContextValue>({} as GlobalContex
 export const GlobalProvider: React.FC<PropsWithChildren> = (props) => {
   const [session, setSession] = useState<Session | null>(null);
   const [branch, setBranch] = useState<any>(null);
+  const [location, setLocation] = useState<Location>({
+    latitude: 37.782,
+    longitude: 29.097,
+    label: 'Konum Seçilmedi',
+  });
 
   useEffect(() => {
     supabase.auth.getSession().then(({ data: { session } }) => {
@@ -45,6 +58,8 @@ export const GlobalProvider: React.FC<PropsWithChildren> = (props) => {
         setSession,
         branch,
         setBranch,
+        location,
+        setLocation,
       }}>
       {props.children}
     </GlobalContext.Provider>
